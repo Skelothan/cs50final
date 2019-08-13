15d85214a038e8a0e33be21c5694cd4d7ee58445
Skelothan <skelothan@gmail.com>
2019-08-12 21:04:30 -0400

    Pot: HP down, score up; Brick: shot spread altered, shot interval down

--- src/BrickEnemy.lua
+++ src/BrickEnemy.lua
@@ -9,7 +9,7 @@ function BrickEnemy:init(x, y)
 		dx = 0,
 		dy = 200,
 		health = 150,
-		score = 400,
+		score = 4000,
 		shot_timer = 3,
 		texture = "breakout",
 		frame = "breakout_bricks",
@@ -21,7 +21,7 @@ function BrickEnemy:update(dt, play_state)
 	self.shot_timer = self.shot_timer - dt
 	if self.shot_timer <= 0 and self.y < VIRTUAL_HEIGHT/2 then
 		self.shoot(self, play_state)
-		self.shot_timer = 3
+		self.shot_timer = 2
 	end
 	if self.y > VIRTUAL_HEIGHT + self.height + 1 then
 		self.destroyed = true
@@ -36,7 +36,7 @@ end
 function BrickEnemy.shoot(self, play_state)
 	local player_x = play_state.player.x + play_state.player.width/2
 	local player_y = play_state.player.y + play_state.player.height/2
-	local speed = 128
+	-- local speed = 128
 	local shot_spread = 0.25 -- about 15 degrees
 	
 	local shot_x = self.x + self.width/2 - 4
@@ -44,22 +44,24 @@ function BrickEnemy.shoot(self, play_state)
 	
 	local theta = math.atan2((player_x - shot_x), (player_y - shot_y))
 	
-	local dy = {speed * math.cos(theta), speed * math.cos(theta - shot_spread), speed * math.cos(theta + shot_spread)}
-	local dx = {speed * math.sin(theta), speed * math.sin(theta - shot_spread), speed * math.sin(theta + shot_spread)}
+	for speed = 96, 128, 16 do
+		local dy = {speed * math.cos(theta), speed * math.cos(theta - shot_spread), speed * math.cos(theta + shot_spread)}
+		local dx = {speed * math.sin(theta), speed * math.sin(theta - shot_spread), speed * math.sin(theta + shot_spread)}
 	
-	for k = 1, 3, 1 do
-		local new_shot = EnemyShot({
-			x = shot_x,
-			y = shot_y,
-			dx = dx[k],
-			dy = dy[k],
-			width = 8,
-			height = 8,
-			texture = "breakout",
-			frame = "breakout_balls",
-			frame_number = 181
-		})
+		for k = 1, 3, 1 do
+			local new_shot = EnemyShot({
+				x = shot_x,
+				y = shot_y,
+				dx = dx[k],
+				dy = dy[k],
+				width = 8,
+				height = 8,
+				texture = "breakout",
+				frame = "breakout_balls",
+				frame_number = 181
+			})
 	
-		table.insert(play_state.enemy_shots, new_shot)
+			table.insert(play_state.enemy_shots, new_shot)
+		end
 	end
 end
